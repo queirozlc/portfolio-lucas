7f490504846c70359650e0f5a2b40012cb1c21a0
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-26 20:44:41 -0300

    feat: homepage structure changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import Header from '@/components/Header'
+import HomeHero from '@/components/HomeHero'
+import { HomeContainer } from '@/styles/HomeStyles'
+
 export default function Home() {
-  return <></>;
+  return (
+    <HomeContainer>
+      <Header />
+
+      <main className="container">
+        <HomeHero />
+      </main>
+    </HomeContainer>
+  )
 }
