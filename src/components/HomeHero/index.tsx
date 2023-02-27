dbce255cafb350c3d8e69a6d082c68514ccb65a7
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-26 22:20:08 -0300

    feat: Home hero component

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,57 @@
+import theme from '@/styles/themes/theme'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import { CodeItem, Container, InfosContainer, TextContainer } from './styles'
+
+export default function HomeHero() {
+  const [texts] = useTypewriter({
+    words: [
+      'Hello World !',
+      '<Lucas Queiroz/>',
+      'Fullstack Developer Jr',
+      'Spring Framework',
+      'Next JS',
+      'TypeScript'
+    ],
+    loop: true,
+    delaySpeed: 2000,
+    typeSpeed: 120
+  })
+
+  return (
+    <Container>
+      {/* <div> */}
+      <TextContainer>
+        <h1>Olá</h1>
+        <h2>
+          {texts}
+          <Cursor cursorColor={`${theme.primary}`} />
+        </h2>
+      </TextContainer>
+      <InfosContainer>
+        <CodeItem>
+          <span className="comment">//Minha apresentação</span>
+          <span className="purple">Infos</span> {'\u007B'}
+          <div>
+            Nome: <span className="blue">'Lucas',</span>
+          </div>
+          <div>
+            Sobrenome: <span className="blue">'Queiroz'</span>
+          </div>
+          {'\u007D'}
+        </CodeItem>
+
+        <CodeItem>
+          <span className="purple">Cargo</span> {'\u007B'}
+          <div>
+            Função: <span className="blue">'Dev Fullstack.',</span>
+          </div>
+          <div>
+            Stacks: <span className="blue">['Java', {'\n'}'React JS']</span>
+          </div>
+          {'\u007D'}
+        </CodeItem>
+      </InfosContainer>
+      {/* </div> */}
+    </Container>
+  )
+}
---
+++ src/components/HomeHero/styles.ts
@@ -0,0 +1,123 @@
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+export const Container = styled.section`
+  ${tw`
+    flex
+    w-full
+    flex-col
+    gap-8
+    items-center
+    justify-center
+    mt-[5rem]
+    max-w-screen-md:flex-col
+  `}
+
+  > div {
+    ${tw`
+      max-w-screen-md:w-full
+    `}
+  }
+`
+
+export const TextContainer = styled.section`
+  ${tw`
+    mb-8
+    w-full
+    text-center
+    max-w-screen-md:text-left
+  `}
+
+  > h1 {
+    ${tw`
+      text-9xl
+      max-w-screen-2xl:text-[5rem]
+      max-w-screen-lg:text-5xl
+      font-bold
+      text-primary
+      mb-10
+    `}
+  }
+
+  > h2 {
+    ${tw`
+      text-5xl
+      max-w-screen-2xl:text-[2rem]
+      max-w-screen-lg:text-2xl
+      text-secondary
+      font-medium
+      font-secondary
+    `}
+  }
+`
+export const InfosContainer = styled.section`
+  ${tw`
+    w-full
+    max-w-4xl
+    max-w-screen-2xl:max-w-2xl
+    max-w-screen-lg:max-w-lg
+    flex
+    flex-col
+    gap-8
+  `}
+`
+
+export const CodeItem = styled.pre`
+  ${tw`
+    p-8
+    text-white
+    w-[24rem]
+    max-w-screen-2xl:w-[18rem]
+    max-w-screen-2xl:p-[1.5rem]
+    max-w-screen-2xl:text-[0.8rem]
+    max-w-screen-lg:w-full
+    self-start
+    transition
+    duration-1000
+    ease-in-out
+    hover:brightness-[1.2]
+  `}
+  background: ${({ theme }) => theme.gradient};
+
+  &:last-child {
+    ${tw`
+      self-end
+      w-[25rem]
+      max-w-screen-2xl:w-[21rem]
+      max-w-screen-2xl:p-[1.5rem]
+      max-w-screen-2xl:text-[0.8rem]
+      max-w-screen-lg:w-full
+    `}
+  }
+
+  > div {
+    ${tw`
+      my-[0.2rem]
+      ml-4
+      font-secondary
+    `}
+  }
+
+  span {
+    ${tw`
+      font-secondary
+      font-medium
+    `}
+  }
+
+  span.purple {
+    color: #c38cdd;
+  }
+
+  span.blue {
+    color: #7ac7e3;
+  }
+
+  span.comment {
+    ${tw`
+      text-text
+      block
+      mb-4
+    `}
+  }
+`
