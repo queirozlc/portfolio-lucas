440b06fdfd5f211817a07f4b0db917c7a0ae9190
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-28 15:50:30 -0300

    feat: client-side mail functionality
    setting up the server-side mail sender on form submit

--- src/components/FormContato/Form/index.tsx
+++ src/components/FormContato/Form/index.tsx
@@ -1,12 +1,74 @@
+import { sendContactMail } from '@/services/sendMail'
+import theme from '@/styles/themes/theme'
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
 import { FormContainer, Input, TextArea } from '../styles'
 
 export function Form() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault()
+
+    if (!name || !email || !message)
+      return toast('Preencha todos os campos !', {
+        style: {
+          background: theme.error,
+          color: '#fff'
+        }
+      })
+
+    try {
+      setLoading(true)
+      await sendContactMail(name, email, message)
+      console.log('Mensagem enviada com sucesso!')
+      setName('')
+      setEmail('')
+      setMessage('')
+
+      toast('Mensagem enviada com sucesso!', {
+        style: {
+          background: theme.backgroundLight,
+          color: theme.primary
+        }
+      })
+    } catch (error) {
+      toast('Erro ao enviar mensagem. Tente novamente :/', {
+        style: {
+          background: theme.error,
+          color: '#fff'
+        }
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <FormContainer data-aos="zoom-out">
-      <Input type="text" placeholder="Nome" required />
-      <Input type="email" placeholder="E-mail" required />
-      <TextArea placeholder="Mensagem" required />
-      <button type="submit">Enviar</button>
+    <FormContainer data-aos="zoom-out" onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        placeholder="Nome"
+        value={name}
+        onChange={({ target }) => setName(target.value)}
+      />
+      <Input
+        type="email"
+        placeholder="E-mail"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <TextArea
+        placeholder="Mensagem"
+        value={message}
+        onChange={({ target }) => setMessage(target.value)}
+      />
+      <button type="submit" disabled={loading}>
+        Enviar
+      </button>
     </FormContainer>
   )
 }
